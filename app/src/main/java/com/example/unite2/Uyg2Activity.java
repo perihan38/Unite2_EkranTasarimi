dc097660431aa850d4992e2a102a4ed42646d3d3
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-09-20 16:54:27 +0300

    no message

---
+++ app/src/main/java/com/example/unite2/Uyg2Activity.java
@@ -0,0 +1,16 @@
+package com.example.unite2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Uyg2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uyg2);
+    }
+}
